a438e2928e7f2acba67c839f78ed2737c4079b27
vignesh <vigneshgs271096@gmail.com>
2024-03-13 11:35:21 +0530

    2nd push

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const newGameBtn = document.querySelector(".btn--new");
 const rollDiceBtn = document.querySelector(".btn--roll");
 const holdBtn = document.querySelector(".btn--hold");
 const diceElement = document.querySelector(".dice");
-
+const resultElement = document.querySelector(".result");
 
 //variables
 const winningScore = 10;
@@ -17,14 +17,15 @@ let score;
 let currentPlayer;
 let currentScore;
 let isPlaying = true;
-
+let winner;
+let firstRoll = true;
 
 init();
 
 // starting function
 function init() {
-  score = [0,0];
-	currentScore = [0,0];
+	score = [0, 0];
+	currentScore = [0, 0];
 	currentPlayer = 0;
 	currentScore[0] = 0;
 	currentScore[1] = 0;
@@ -41,40 +42,59 @@ function restoreScores() {
 }
 
 // role dice
-rollDiceBtn.addEventListener('click',function(){
-	let randomNum = Math.trunc((Math.random() * 6) + 1);
-	console.log(score,currentScore)
-	diceElement.classList.remove("hide");
-	diceElement.src = `dice-${randomNum}.png`;	
-	if(randomNum % 2 == 0) {
-		switchPlayer();
-	} else {
-		currentScore[currentPlayer] += randomNum;
+rollDiceBtn.addEventListener('click', function () {
+	if(isPlaying) {
+		let randomNum = Math.trunc((Math.random() * 6) + 1);
+		diceElement.src = `dice-${randomNum}.png`;
+		if (randomNum % 2 == 0) {
+			switchPlayer();
+		} else {
+			currentScore[currentPlayer] += randomNum;
+		}
+		randomNum = 0;
+		restoreScores();
 	}
-	randomNum = 0;
-	restoreScores();
+	if(firstRoll) {
+		diceElement.classList.remove("hide");
+		firstRoll = false;
+	}
 })
 
 // hold score
-holdBtn.addEventListener('click',function(){
-	score[currentPlayer] += currentScore[currentPlayer];
-	currentScore[currentPlayer] = 0;
-	restoreScores();
-	// if(score[currentPlayer] >= winningScore) {
-	// 	isPlaying = false;
-	// }
-	switchPlayer();
+holdBtn.addEventListener('click', function () {
+	if(isPlaying) {
+		score[currentPlayer] += currentScore[currentPlayer];
+		currentScore[currentPlayer] = 0;
+		restoreScores();
+		if(score[currentPlayer] > winningScore) {
+			gameEnd();
+		}
+		switchPlayer();
+	}
 })
 
 // switch player
 function switchPlayer() {
 	currentScore[currentPlayer] = 0;
 	document.querySelector(`.player--${currentPlayer}`).classList.remove("player--active");
-	currentPlayer = (currentPlayer == 0)? 1 : 0;
+	currentPlayer = (currentPlayer == 0) ? 1 : 0;
 	document.querySelector(`.player--${currentPlayer}`).classList.add("player--active");
 };
 
 // new game
-newGameBtn.addEventListener('click',function(){
+newGameBtn.addEventListener('click', function () {
 	init();
+	resultElement.classList.add("opacity");
+	isPlaying = true;
+	firstRoll = true;
 })
+
+// game end
+function gameEnd() {
+	isPlaying = false;
+	winner = currentPlayer;
+	resultElement.textContent = `Player${ currentPlayer + 1 } wins...`;
+	diceElement.classList.add("hide");
+	resultElement.classList.remove("opacity");
+}
+
